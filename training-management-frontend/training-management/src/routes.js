75baa8bd6b913cb7a91ab9f62901eae380e63ec9
Roshandeep Barik <rohanbarik292@gmail.com>
2024-10-30 22:36:01 +0530

    (fix):fix naming convention and bugs in routes.js

--- training-management-frontend/training-management/src/routes.js
+++ training-management-frontend/training-management/src/routes.js
@@ -1,43 +1,43 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import LandingPage from './Components/Common/LandingPage';
-import Login from './Components/Common/Login';
-import AdminDashboard from './Components/Admin/AdminDashboard';
-import ManageTraining from './Components/Admin/ManageTraining';
-import PendingApprovals from './Components/Admin/PendingApprovals';
-import ManagerDashboard from './Components/Manager/ManagerDashboard';
-import PendingRequests from './Components/Manager/PendingRequests';
-import RequestDetails from './Components/Manager/RequestDetails';
-import TraineeDashboard from './Components/Trainee/TraineeDashboard';
-import OngoingTraining from './Components/Trainee/OngoingTraining';
-import CompletedTraining from './Components/Trainee/CompletedTraining';
-import AvailableTraining from './Components/Trainee/AvailableTraining';
-import RequestNewTraining from './Components/Trainee/RequestNewTraining';
-import TrainerDashboard from './Components/Trainer/TrainerDashboard';
-import PastTraining from './Components/Trainer/PastTraining';
-import ParticipantInformation from './Components/Trainer/ParticipantInformation';
+import { BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+import LandingPage from './components/common/landing-page/landingPage';
+import Login from './components/common/login/login';
+import AdminDashboard from './components/admin/adminDashboard';
+import ManageTraining from './components/admin/manageTraining';
+import PendingApprovals from './components/admin/pendingApprovals';
+import ManagerDashboard from './components/manager/managerDashboard';
+import PendingRequests from './components/manager/pendingRequests';
+import RequestDetails from './components/manager/requestDetails';
+import TraineeDashboard from './components/trainee/traineeDashboard';
+import OngoingTraining from './components/trainee/ongoingTraining';
+import CompletedTraining from './components/trainee/completedTraining';
+import AvailableTraining from './components/trainee/availableTraining';
+import RequestNewTraining from './components/trainee/requestNewTraining';
+import TrainerDashboard from './components/trainer/trainerDashboard';
+import PastTraining from './components/trainer/pastTraining';
+import ParticipantInformation from './components/trainer/participantInformation';
 
-const Routes = () => (
+const AppRoutes = () => (
   <Router>
-    <Switch>
-      <Route path="/" exact component={LandingPage} />
-      <Route path="/login" component={Login} />
-      <Route path="/admin/dashboard" component={AdminDashboard} />
-      <Route path="/admin/manage-training" component={ManageTraining} />
-      <Route path="/admin/pending-approvals" component={PendingApprovals} />
-      <Route path="/manager/dashboard" component={ManagerDashboard} />
-      <Route path="/manager/pending-requests" component={PendingRequests} />
-      <Route path="/manager/request-details/:id" component={RequestDetails} />
-      <Route path="/trainee/dashboard" component={TraineeDashboard} />
-      <Route path="/trainee/ongoing-training" component={OngoingTraining} />
-      <Route path="/trainee/completed-training" component={CompletedTraining} />
-      <Route path="/trainee/available-training" component={AvailableTraining} />
-      <Route path="/trainee/request-new-training" component={RequestNewTraining} />
-      <Route path="/trainer/dashboard" component={TrainerDashboard} />
-      <Route path="/trainer/past-training" component={PastTraining} />
-      <Route path="/trainer/participant-information/:id" component={ParticipantInformation} />
-    </Switch>
+    <Routes>
+      <Route path="/" element={<LandingPage/>} exact />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/admin/dashboard" element={<AdminDashboard/>} />
+      <Route path="/admin/manage-training" element={<ManageTraining/>} />
+      <Route path="/admin/pending-approvals" element={<PendingApprovals/>} />
+      <Route path="/manager/dashboard" element={<ManagerDashboard/>} />
+      <Route path="/manager/pending-requests" element={<PendingRequests/>} />
+      <Route path="/manager/request-details/:id" element={<RequestDetails/>} />
+      <Route path="/trainee/dashboard" element={<TraineeDashboard/>} />
+      <Route path="/trainee/ongoing-training" element={<OngoingTraining/>} />
+      <Route path="/trainee/completed-training" element={<CompletedTraining/>} />
+      <Route path="/trainee/available-training" element={<AvailableTraining/>} />
+      <Route path="/trainee/request-new-training" element={<RequestNewTraining/>} />
+      <Route path="/trainer/dashboard" element={<TrainerDashboard/>} />
+      <Route path="/trainer/past-training" element={<PastTraining/>} />
+      <Route path="/trainer/participant-information/:id" element={<ParticipantInformation/>} />
+      </Routes>
   </Router>
 );
 
-export default Routes;
+export default AppRoutes;
