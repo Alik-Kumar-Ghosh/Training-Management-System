2ee850cc20ec97109972230f5b9c61a83f1735de
Ashutosh74554 <ashutoshdash1437@gmail.com>
2024-10-31 16:44:30 +0530

    (feat): Implement user service and add required custom queries on repository interfaces

--- training-management-backend/src/main/java/com/training/repo/UserRepo.java
+++ training-management-backend/src/main/java/com/training/repo/UserRepo.java
@@ -1,10 +1,13 @@
 package com.training.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 import com.training.model.User;
 
 @Repository
 public interface UserRepo extends JpaRepository<User, Integer>{
-
+	@Query("select u from User u where u.name = :name")
+	List<User> findByName(String name);
 }
