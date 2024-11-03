54374333ba5da0b0a757c320f212a0d92f3526cc
Ashutosh74554 <ashutoshdash1437@gmail.com>
2024-11-04 01:28:22 +0530

    (feat): Add all required api by frontend

--- training-management-backend/src/main/java/com/training/controller/ApplicationNotFoundException.java
+++ training-management-backend/src/main/java/com/training/controller/ApplicationNotFoundException.java
@@ -4,10 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class TrainingNotFoundException extends RuntimeException {
-	private static final long serialVersionUID = 2L;
+public class ApplicationNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 4L;
 
-	public TrainingNotFoundException(String message) {
+	public ApplicationNotFoundException(String message) {
         super(message);
     }
 }
