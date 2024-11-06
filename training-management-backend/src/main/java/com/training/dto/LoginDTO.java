821fbb4f44fa719a18c9ba21ed5d47b53ece0858
Ashutosh74554 <ashutoshdash1437@gmail.com>
2024-11-06 22:56:17 +0530

    (feat): Use LoginDTO object in login API and set password data member in User entity to JsonIgnore

---
+++ training-management-backend/src/main/java/com/training/dto/LoginDTO.java
@@ -0,0 +1,27 @@
+package com.training.dto;
+
+public class LoginDTO {
+	private String userName;
+	private String password;
+
+	public LoginDTO(){
+		super();
+	}
+	public LoginDTO(String userName, String password) {
+		this.userName = userName;
+		this.password = password;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
